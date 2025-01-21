a3e8f080ae43391b259b961b4f4e20410ca99206
Noah Netz <noahjnetz@gmail.com>
2025-01-21 13:21:53 -0700

    Interrupt LED

--- app/main.asm
+++ app/main.asm
@@ -23,6 +23,21 @@ init:
             ; Disable low-power mode
             bic.w   #LOCKLPM5,&PM5CTL0
 
+            bic.b   #BIT6,&P6OUT            ; Clear P6.6 output
+            bis.b   #BIT6,&P6DIR            ; P6.6 output
+            bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
+
+            bis.w   #TBCLR,&TB0CTL          ; Clear timer
+            bis.w   #TBSSEL__SMCLK,&TB0CTL  ; Select SMCLK as timer source
+            bis.w   #MC__UP,&TB0CTL         ; UP counting
+            bis.w   #ID__4,&TB0CTL          ; Div-by-4
+            mov.w   #111,&TB0EX0              ; Divide by 8
+
+            mov.w   #32150,&TB0CCR0
+            bis.w   #CCIE,&TB0CCTL0
+            bic.w   #CCIFG,&TB0CCTL0
+            bis.w   #GIE,SR
+
 main:
 
             nop 
@@ -32,7 +47,19 @@ main:
 
 
 ;------------------------------------------------------------------------------
+;           ISR
+;------------------------------------------------------------------------------
+ISR_TB0_Overflow:
+            xor.b   #BIT6,&P6OUT
+            bic.w   #CCIFG,&TB0CCTL0
+            reti
+            
+;------------------------------------------------------------------------------
 ;           Interrupt Vectors
 ;------------------------------------------------------------------------------
             .sect   RESET_VECTOR            ; MSP430 RESET Vector
             .short  RESET                   ;
+
+            .sect   ".int43"
+            .short  ISR_TB0_Overflow
+            .end
