99086c25242bfa874d86558dafe4c7dc2f39b572
Noah Netz <noahjnetz@gmail.com>
2025-02-04 13:38:46 -0700

    GOT rx_ACK WORKING!!!!

--- app/main.asm
+++ app/main.asm
@@ -65,23 +65,23 @@ main:
             call    #tx_start               ; start condition
             mov.b   #01010100b, tx_address  ; Set Address
             call    #i2c_tx_address         ; Transmit Address
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
             
             mov.b   #48h, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
             
             mov.b   #69h, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
             
             mov.b   #3Fh, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
 
             call    #tx_stop                ; stop condition
 
-            call    #tx_count
+            ;call    #tx_count
 
             jmp     main
             nop
@@ -178,18 +178,18 @@ tx_count
             call    #tx_start               ; start condition
             mov.b   #01010100b, tx_address  ; Set Address
             call    #i2c_tx_address         ; Transmit Address
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
 tx_count_loop
             mov.b   R11, tx_byte            ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #tx_ACK                 ; Send Ack
+            call    #rx_ACK                 ; Send Ack
 
             dec.b   R11
             jnz     tx_count_loop           ; loop if counter is not 0
             
             call    #tx_stop                ; stop condition
             
-; -- simulate start condition
+; -- simulate ack
 tx_ACK
             ;SDA to LOW then pulse SCL
             bic.b   #BIT0,&P2OUT            ; Set SDA to LOW
@@ -199,23 +199,27 @@ tx_ACK
             bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
             ret
 
-; -- receive actual start condition
+; -- receive actual ack
 rx_ACK
+
             ;receive ACK from AD2
             bic.b   #BIT0,&P2DIR            ; P2.0 input (SDA)
             bis.b   #BIT0,&P2REN            ; P2.0 enable resistor
             bis.b   #BIT0,&P2OUT            ; P2.0 pullup resistor
+            
             call    #Delay
-
+            
             bis.b   #BIT2,&P2OUT            ; Set SCL to HIGH
             call    #Delay
+
             ; bitmask to read value of 2.0
             mov.b   &P2IN, R12
             inv.b   R12
             and.b   #BIT0, R12
+
             bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
             call    #Delay
-            jnz      rx_ACK_LOW
+            jz      rx_ACK_LOW
 rx_ACK_HIGH
             call    #tx_stop
             jmp     rx_ACK_END
