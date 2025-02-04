3b0e01e180347d79db20ae16039578d25f4145db
Gunnar <rasmussengunnarc@gmail.com>
2025-02-04 01:34:29 -0700

    code refactoring, ack request still not fully working so I'm substituting in the placeholder one

--- app/main.asm
+++ app/main.asm
@@ -62,25 +62,27 @@ init:
 main:
 
             nop 
-            xor.b   #BIT0,&P1OUT
-            call    #START                  ; Call START subroutine
+            call    #tx_start               ; start condition
             mov.b   #01010100b, tx_address  ; Set Address
-            call    #i2c_tx_byte            ; Transmit Address
-            call    #rx_ACK                 ; Send Ack
+            call    #i2c_tx_address         ; Transmit Address
+            call    #tx_ACK                 ; Send Ack
             
-            mov.b   #48h, tx_address        ; Set Data
+            mov.b   #48h, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #rx_ACK                 ; Send Ack
+            call    #tx_ACK                 ; Send Ack
             
-            mov.b   #69h, tx_address        ; Set Data
+            mov.b   #69h, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #rx_ACK                 ; Send Ack
+            call    #tx_ACK                 ; Send Ack
             
-            mov.b   #3Fh, tx_address        ; Set Data
+            mov.b   #3Fh, tx_byte           ; Set Data
             call    #i2c_tx_byte            ; Transmit Data
-            call    #rx_ACK                 ; Send Ack
+            call    #tx_ACK                 ; Send Ack
 
-            call    #STOP                   ; STOP
+            call    #tx_stop                ; stop condition
+
+            call    #tx_count
+
             jmp     main
             nop
 
@@ -93,6 +95,7 @@ main:
 
 tx_address: .ubyte  00h                        ; Create variable tx_address
 tx_byte:    .ubyte  00h                        ; Create variable tx_byte
+count:      .ubyte  00h                        ; Create count variable
 
 
 ;------------------------------------------------------------------------------
@@ -103,19 +106,48 @@ Inner_loop  dec.w   R14
             jnz     Inner_loop
             ret
 
-START       bic.b   #BIT0,&P2OUT            ; Set SDA to LOW
+; --- transmit start condition
+tx_start    bic.b   #BIT0,&P2OUT            ; Set SDA to LOW
             call    #Delay                  ; Call delay subroutine
             bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
             ret
 
-STOP        bis.b   #BIT2,&P2OUT            ; Set SCL to HIGH
+; -- transmit stop condition
+tx_stop     bis.b   #BIT2,&P2OUT            ; Set SCL to HIGH
             call    #Delay                  ; Call delay subroutine
             bis.b   #BIT0,&P2OUT            ; Set SDA to HIGH
             ret
 
+; -- transmit address
+i2c_tx_address 
+            mov.w   #0008h, R13
+ad_byte_lp  rlc.b   tx_address
+            jlo     ad_bit_0
+            jc      ad_bit_1
+ad_bit_0
+            bic.b   #BIT0,&P2OUT            ; Set SDA to LOW
+            call    #Delay
+            bis.b   #BIT2,&P2OUT            ; Set SCL to HIGH
+            call    #Delay
+            bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
+            call    #Delay
+            jmp     ad_bit_end
+ad_bit_1
+            bis.b   #BIT0,&P2OUT            ; Set SDA to HIGH
+            call    #Delay
+            bis.b   #BIT2,&P2OUT            ; Set SCL to HIGH
+            call    #Delay
+            bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
+            call    #Delay
+ad_bit_end
+            dec.w   R13
+            jnz     ad_byte_lp
+            ret
+
+; -- transmit byte
 i2c_tx_byte 
-            mov.w  #0008h, R13
-byte_loop   rlc.b   tx_address
+            mov.w   #0008h, R13
+byte_loop   rlc.b   tx_byte
             jlo     tx_bit_0
             jc      tx_bit_1
 tx_bit_0
@@ -138,6 +170,26 @@ tx_bit_end
             jnz     byte_loop
             ret
 
+; -- transmit multiple bytes (count)
+tx_count
+            mov.b   #09h, count             ; define count variable
+            mov.b   count, R11              ; put count into register
+
+            call    #tx_start               ; start condition
+            mov.b   #01010100b, tx_address  ; Set Address
+            call    #i2c_tx_address         ; Transmit Address
+            call    #tx_ACK                 ; Send Ack
+tx_count_loop
+            mov.b   R11, tx_byte            ; Set Data
+            call    #i2c_tx_byte            ; Transmit Data
+            call    #tx_ACK                 ; Send Ack
+
+            dec.b   R11
+            jnz     tx_count_loop           ; loop if counter is not 0
+            
+            call    #tx_stop                ; stop condition
+            
+; -- simulate start condition
 tx_ACK
             ;SDA to LOW then pulse SCL
             bic.b   #BIT0,&P2OUT            ; Set SDA to LOW
@@ -147,6 +199,7 @@ tx_ACK
             bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
             ret
 
+; -- receive actual start condition
 rx_ACK
             ;receive ACK from AD2
             bic.b   #BIT0,&P2DIR            ; P2.0 input (SDA)
@@ -160,14 +213,14 @@ rx_ACK
             mov.b   &P2IN, R12
             inv.b   R12
             and.b   #BIT0, R12
-            jz      rx_ACK_LOW
+            bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
+            call    #Delay
+            jnz      rx_ACK_LOW
 rx_ACK_HIGH
-            call    #STOP
+            call    #tx_stop
             jmp     rx_ACK_END
 rx_ACK_LOW
 rx_ACK_END
-            bic.b   #BIT2,&P2OUT            ; Set SCL to LOW
-            call    #Delay
             bic.b   #BIT0,&P2OUT            ; Clear P2.0 output
             bis.b   #BIT0,&P2DIR            ; P2.0 output (SDA)
             bis.b   #BIT0,&P2OUT            ; Set P2.0 to HIGH
